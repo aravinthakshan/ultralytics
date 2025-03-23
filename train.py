e7ee6aa46287d34130639e51383001ba48fac0f5
aravinthakshan <aaravinth2005@gmail.com>
2025-03-23 15:15:38 +0530

    100 epochs

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ model.add_callback('on_train_batch_end', log_losses)
 
 # Train the model with the specified configuration and sync to W&B
 Result_Final_model = model.train(
-    epochs=5,
+    epochs=100,
     data="coco128.yaml",
     optimizer='SOAP',
     project='rtdetr_new_exp',
